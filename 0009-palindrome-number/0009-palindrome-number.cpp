fddcc07937d27e076145ed77de210427ce0a2492
Harsh nagavan <nagwanharshu@gmail.com>
2026-08-11 22:18:15 +0530

    Time: 3 ms (44.15%), Space: 8.4 MB (92.78%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int reverse(int n) {
+        int rev = 0;
+        while (n != 0) {
+            int digit = n % 10;
+            if (rev > INT_MAX / 10 || rev < INT_MIN / 10) {
+                return 0;
+            }
+            rev = rev * 10 + digit;
+            n = n / 10;
+        }
+        return rev;
+    }
+
+    bool isPalindrome(int n) {
+        if (n < 0) return false;
+        int rev = reverse(n);
+        return n == rev;
+    }
+};
